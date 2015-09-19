ce4ae0f4aa24d369b35c14a369097751bb1139d3
Geoffrey Challen <challen@buffalo.edu>
2015-09-19 11:32:04 -0400

    Starting testing.

---
+++ test/index.js
@@ -0,0 +1,77 @@
+require('harmonize')(['harmony-generators']);
+
+var metalsmith = require('metalsmith'),
+    fs = require('fs'),
+    path = require('path'),
+    _ = require('underscore'),
+    chai = require('chai'),
+    jsonfile = require('jsonfile'),
+    async = require('async'),
+    spellcheck = require('..'),
+    spellcheckDefaults = require('../lib/spellcheckDefaults.js');
+
+chai.use(require('chai-fs'));
+var assert = chai.assert;
+
+function reset_files(test_defaults) {
+  try {
+    fs.unlinkSync(test_defaults.checkFile);
+  } catch (err) {};
+  try {
+    fs.unlinkSync(test_defaults.failFile);
+  } catch (err) {};
+  try {
+    fs.unlinkSync(test_defaults.exceptionFile);
+  } catch (err) {};
+  assert.notPathExists(test_defaults.exceptionFile);
+  assert.notPathExists(test_defaults.checkFile);
+  assert.notPathExists(test_defaults.failFile);
+}
+
+function check_files(files, defaults) {
+  assert(!(defaults.checkFile in files));
+  assert(!(defaults.failFile in files));
+  assert(!(defaults.exceptionFile in files));
+}
+
+function defaultsWithDictionary() {
+  var defaults = _.clone(spellcheckDefaults.defaults);
+  defaults.dicFile = 'en_US.dic';
+  defaults.affFile = 'en_US.aff';
+  return defaults;
+}
+
+describe('metalsmith-spellcheck', function() {
+  it('should identify all misspelled words with the default parameters', function(done) {
+    var src = 'test/fixtures/errors';
+    var defaults = defaultsWithDictionary();
+    var test_defaults = spellcheckDefaults.processConfig(defaults, path.join(src, 'src'));
+    reset_files(test_defaults);
+
+    metalsmith(src)
+      .use(spellcheck(defaults))
+      .build(function (err, files) {
+        if (err) {
+          return done(err);
+        }
+        done();
+      });
+  });
+  it('should ignore misspelled words in the exception file', function(done) {
+    var src = 'test/fixtures/errors';
+    var defaults = defaultsWithDictionary();
+    var test_defaults = spellcheckDefaults.processConfig(defaults, path.join(src, 'src'));
+    var exceptions = { "Geoffrey": true, "/^Challen$/": true };
+    reset_files(test_defaults);
+    jsonfile.writeFileSync(test_defaults.exceptionFile, exceptions);
+
+    metalsmith(src)
+      .use(spellcheck(defaults))
+      .build(function (err, files) {
+        if (err) {
+          return done(err);
+        }
+        done();
+      });
+  });
+});
